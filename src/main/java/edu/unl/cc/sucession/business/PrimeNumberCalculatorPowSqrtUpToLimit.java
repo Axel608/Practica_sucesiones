f8b744dddb57df66ac4f9b5d9a6d63089ebfb859
Axel608 <axellapo33@gmail.com>
2025-12-25 15:48:40 -0500

    Update PrimeNumberCalculatorPowSqrtUpToLimit.java
    puesta de autores

--- src/main/java/edu/unl/cc/sucession/business/PrimeNumberCalculatorPowSqrtUpToLimit.java
+++ src/main/java/edu/unl/cc/sucession/business/PrimeNumberCalculatorPowSqrtUpToLimit.java
@@ -1,5 +1,13 @@
 package edu.unl.cc.sucession.business;
 
+/**
+ * Autores
+ * Joel Tene 
+ * Teresa Rivas
+ * Axel Lapo
+ * Jorge Noboa
+ */
+
 public class PrimeNumberCalculatorPowSqrtUpToLimit {
 
     private Integer limit;
