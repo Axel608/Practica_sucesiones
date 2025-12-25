71635fddb90c8701339df4ec1f094c735f867769
Axel608 <axellapo33@gmail.com>
2025-12-25 15:49:02 -0500

    Update PrimeNumberWithPowCalculator.java

--- src/main/java/edu/unl/cc/sucession/business/PrimeNumberWithPowCalculator.java
+++ src/main/java/edu/unl/cc/sucession/business/PrimeNumberWithPowCalculator.java
@@ -1,5 +1,13 @@
 package edu.unl.cc.sucession.business;
 
+/**
+ * Autores
+ * Joel Tene 
+ * Teresa Rivas
+ * Axel Lapo
+ * Jorge Noboa
+ */
+
 public class PrimeNumberWithPowCalculator {
    private Integer limit;
     private Integer currentTerm;
